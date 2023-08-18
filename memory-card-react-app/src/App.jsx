d95a94c8d5e301bdf6ca4d9b2cd7fe89528ff260
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-08-18 15:19:23 +0300

    I fetch some giphs from the api, trying to display them after the user presses the start the game button

--- memory-card-react-app/src/App.jsx
+++ memory-card-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Card from "./components/card";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Menu from "./components/menu";
 
 const App = () => {
     const [isCardSelected, setCardSelected] = useState(false);
@@ -9,11 +10,34 @@ const App = () => {
         score: 0,
         maxSore: 0,
         won: false,
+        gameIsLive: false,
     });
+    const [images, setImages] = useState([]);
+    const cards = [];
+
+    async function fetchAPI() {
+        const response = await fetch(
+            "https://api.giphy.com/v1/gifs/search?api_key=BHUOF7tIpJB747L5NMZfskq0fA07yJH0&q=naruto&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+        );
+        const data = await response.json();
+        data.data.map((elem) => {
+            cards.push({ id: elem.id, url: elem.url, title: elem.title });
+        });
+        setImages(cards);
+    }
 
     function handleClick() {
         setCardSelected((isCardSelected) => !isCardSelected);
         console.log(isCardSelected);
+    }
+
+    function gameStartClick() {
+        setGameState((prevState) => ({
+            ...prevState,
+            gameIsLive: true,
+        }));
+        fetchAPI();
+        console.log(images);
     }
 
     return (
@@ -29,6 +53,10 @@ const App = () => {
             >
                 Click
             </button>
+            <Menu gameStart={gameStartClick}></Menu>
+            {images.map((elem) => {
+                return <Card name={elem.title}></Card>;
+            })}
             <Card
                 name="A"
                 isCardSelected={isCardSelected}
---
+++ memory-card-react-app/src/components/menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div>
+            <button>Start Game</button>
+        </div>
+    );
+};
+
+export default Menu;
