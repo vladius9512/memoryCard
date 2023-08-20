cd42c99de01ad0e4b6f60fffb914fadaa1cefa3a
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-08-20 10:35:32 +0300

    added a shuffle function that works for arrays

--- memory-card-react-app/src/App.jsx
+++ memory-card-react-app/src/App.jsx
@@ -48,8 +48,24 @@ const App = () => {
             }
             return image;
         });
+        shuffleArray(newList);
         setImages(newList);
         console.log(images);
+    }
+
+    function shuffleArray(array) {
+        let currentIndex = array.length,
+            randomIndex;
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+
+        return array;
     }
 
     const checkIfClicked = (data) => {
